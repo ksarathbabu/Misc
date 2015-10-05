f042258963cd0e91a7367c34f71799eb107d9ffb
ksarathbabu <sarathbabu.krishnamurthy@gmail.com>
2015-10-05 12:45:51 -0400

    Update BASH.sh

--- BASH.sh
+++ BASH.sh
@@ -7,3 +7,6 @@ rename 's/^regex or string/change text/' filename
 #find the count of a particular column 
 #-d is the seperator, -f n is the column number
 cut -d "" -f n filename | sort | uniq -c
+#Using cut with tab as delimiter
+cut -d$'\t' -f3,5 filename
+
