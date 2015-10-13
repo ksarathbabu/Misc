e85dd2efdddf8028dd9556ca61c82ee393d9f556
ksarathbabu <sarathbabu.krishnamurthy@gmail.com>
2015-10-13 09:39:13 -0400

    Update BASH.sh

--- BASH.sh
+++ BASH.sh
@@ -10,3 +10,16 @@ cut -d "" -f n filename | sort | uniq -c
 #Using cut with tab as delimiter
 cut -d$'\t' -f3,5 filename
 
+
+#Comparing two files after sorting 
+diff <(sort file1) <(sort file2)
+
+#Make sure both a and b files are sorted 
+#Show lines that only exist in file a: (i.e. what was deleted from a)
+comm -23 a b
+
+#Show lines that only exist in file b: (i.e. what was added to b)
+comm -13 a b
+
+#Show lines that only exist in one file or the other: (but not both)
+comm -3 a b
