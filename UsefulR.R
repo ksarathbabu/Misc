25207bbf25c140b933ca1f03c2dbf06cd0f9bf8d
ksarathbabu <sarathbabu.krishnamurthy@gmail.com>
2019-01-10 16:26:13 -0500

    Update UsefulR.R

--- UsefulR.R
+++ UsefulR.R
@@ -1,3 +1,9 @@
 Find the max value of column per group in a dataframe
 dt <- data.table(d)
 ds=dt[ , max(VALUE), by = GROUP_ID]
+
+#To remove digits from a vector
+gsub("[[:digit:]]","",variable)
+
+#To remove alphabet from a vector
+gsub("[aA-zZ]","",variable)
